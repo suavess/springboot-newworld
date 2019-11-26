5a41012234c7d744d389787c4be0b6baba5be84c
Suave <1132888093@qq.com>
2019-11-26 09:59:51 +0800

    init

---
+++ src/main/java/com/suave/new_world/NewWorldApplication.java
@@ -0,0 +1,13 @@
+package com.suave.new_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suave/new_world/NewWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suave.new_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
